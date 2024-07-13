7bd835ef6d7ad802567673503206f3ef8d007f78
Md Faizul karim <faizulkarim28@gmail.com>
2024-07-13 17:20:54 +0600

    Initial Commit

---
+++ FruitsWorld/FruitsWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsWorldApp.swift
+//  FruitsWorld
+//
+//  Created by Md Faizul karim on 13/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
