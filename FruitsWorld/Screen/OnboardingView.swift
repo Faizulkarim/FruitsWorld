62dd659cd75ea340dd8d146a7ecc9e75240b755f
Md Faizul karim <faizulkarim28@gmail.com>
2024-07-14 00:04:17 +0600

    Created fruitesWord model and added data

---
+++ FruitsWorld/Screen/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  FruitsWorld
+//
+//  Created by Md Faizul karim on 13/7/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
